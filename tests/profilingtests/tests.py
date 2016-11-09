eccf1b8f0c6fe9290b642ad3a18bf8ab681b8efd
Steve Steiner <ssteinerX@gmail.com>
2016-11-09 09:33:19 -0500

    Replace xrange with range in test.  No xrange in Py3.x, not worth fussing over

--- tests/profilingtests/tests.py
+++ tests/profilingtests/tests.py
@@ -31,7 +31,7 @@ class ResourceProfilingTestCase(ProfilingTestCase):
 
         user = User.objects.create_user('foo', 'pass')
 
-        for i in xrange(0, 200):
+        for i in range(0, 200):
             Note.objects.create(author=user, title='Note #%s' % i,
                 slug='note-%s' % i)
 
@@ -41,5 +41,5 @@ class ResourceProfilingTestCase(ProfilingTestCase):
         get_list = self.resource.get_list
         request = self.request
 
-        for i in xrange(0, 50):
+        for i in range(0, 50):
             get_list(request)
